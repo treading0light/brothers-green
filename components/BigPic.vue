c990e1a7829fb37932e32ba85e6be83b162b2c7b
tony0g.lm <tony0light@gmail.com>
2022-08-11 21:29:10 -0700

    initial commit, installed with tailwindcss and daisyui, created some components

---
+++ components/BigPic.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="flex h-screen w-full items-center justify-around">
+
+		<div class="w-1/3 flex flex-col items-center gap-10 bg-base-300">
+			<h2>This picture is hard coded in</h2>
+			<img class="w-4/5 mb-20" src="assets/images/350z.jpg">		
+		</div>
+
+		<div class="w-1/3 flex flex-col items-center gap-10 bg-base-300">
+			<h2>{{ message }}</h2>
+			<img class="w-4/5 mb-20" v-bind:src="url">
+		</div>
+
+
+		
+	</div>
+	
+</template>
+
+<script setup>
+	const props = defineProps({
+		message: String,
+		url: String
+	})
+
+
+</script>
---
+++ components/Intro.vue
@@ -0,0 +1,20 @@
+<template>
+
+	<div id="intro" class="h-screen w-full flex justify-center items-center">
+
+    <h1 class="font-bold text-white text-5xl">{{ message }}</h1>
+
+  </div>
+</template>
+
+<script setup>
+	const message = ref(null)
+
+	const getMessage = () => {
+    return 'Hello Josh Green'
+  }
+
+  onMounted(() => {
+    message.value = getMessage()
+  })
+</script>
