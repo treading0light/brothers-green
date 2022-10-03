a1074b5ad43f17c466299c8b4f38a9672d06c4fd
tony0g <tony0light@gmail.com>
2022-10-02 21:44:16 -0700

    commiting for the sake of deploying on vercel

--- pages/text.vue
+++ pages/text.vue
@@ -1,27 +1,37 @@
 <template>
-	<div>
-		<h1>Here is a blog that I am building.</h1>
+	<div class="min-h-screen w-full bg-cyan-300">
 
-		<p>I will also try to save some v bindings here in this markdown.</p>
-
-		<p>{{ message }}</p>
-
-		<input type="text" v-model="message">
+		<img class="w-20" src="/images/abstract_banner1.jpeg">
 
 
+		<div id="fog" @mousemove="handleMouseMove" class="min-h-screen w-full fixed top-0">
+			<NuxtLink to="/blog" class="btn btn-primary">Exit</NuxtLink>
+		</div>
 	</div>
 
 
 </template>
 
 <script setup>
-		const message = ref(null)
 
-		message.value = 'welcome to the jungle!'
 
-		const changeMessage = (e) => {
-			message.value = e.target.value
-			console.log('message changed!')
-		}
+	const handleMouseMove = (e) => {
+		// console.log(event.clientX)
+		let x = e.clientX.toString() + 'px' 
+	    let y = e.clientY.toString() + 'px'
+	    let xy = x + " " + y
 
+	    const el = document.getElementById('grid')
+
+	    let radValue = "radial-gradient(circle at "+ xy + ", rgba(0,0,0,.25) 100px, rgba(80,90,100,1) 300px )";
+	    let image = ""
+	    e.target.style.backgroundImage = radValue
+	}
 </script>
+
+<style>
+	#fog {
+		background: radial-gradient(circle at 50% 50%, rgba(0,0,0,0) 50px,
+			rgba(80,90,100,1) 50px);
+	}
+</style>
