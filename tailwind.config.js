6deaa9d689d89ab6fd9ce73f1b27c8d43aab32c5
tony0g.lm <tony0light@gmail.com>
2022-10-14 21:03:42 -0700

    fix attempt, reverting tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   purge: [
-     './components/**/*.{vue,js}',
-    './layouts/**/*.vue',
-    './pages/**/*.vue',
-    './plugins/**/*.{js,ts}',
-    './nuxt.config.{js,ts}',
   ],
   content: [
     "./components/**/*.{js,vue,ts}",
