bfa3e3d71fc716fdf1563fc7a8362ef369f12652
tony0g <tony0light@gmail.com>
2022-11-24 10:51:51 -0800

    testing hosted version without client-only component

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -5,9 +5,9 @@
 
 		<article class="prose w-full lg:prose-xl prose-code:bg-base-300 prose-code:text-base-content prose-code:p-5 prose-code:rounded-none prose-code:block prose-code:mb-20">
 
-			<client-only>
+			
 				<ContentDoc :path="slug" />
-			</client-only>
+			
 
 		</article>
 
